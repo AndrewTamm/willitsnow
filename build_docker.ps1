f2bd83a1410450f0f3ebf588db4d0345dbba4385
Andrew Tamm <ahtamm@gmail.com>
2026-03-13 23:10:51 -0700

    Trigger ECS redeployment automatically after image push
    After pushing a new image to ECR, look up the ECS cluster and service
    names from the CloudFormation stack and force a new deployment so the
    running container is updated without a manual step.

--- build_docker.ps1
+++ build_docker.ps1
@@ -15,3 +15,12 @@ docker build --build-arg GTAG=$gtag --build-arg LOCATION=$location -t willitsnow
 docker tag willitsnow:latest "$accountId.dkr.ecr.$region.amazonaws.com/willitsnow_private:latest"
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin "$accountId.dkr.ecr.$region.amazonaws.com"
 docker push "$accountId.dkr.ecr.$region.amazonaws.com/willitsnow_private:latest"
+
+$stackName = "willitsnowinsea"
+$resources = aws cloudformation describe-stack-resources --stack-name $stackName --region $region | ConvertFrom-Json
+
+$ecsCluster = ($resources.StackResources | Where-Object { $_.ResourceType -eq "AWS::ECS::Cluster" } | Select-Object -First 1).PhysicalResourceId
+$ecsService = ($resources.StackResources | Where-Object { $_.ResourceType -eq "AWS::ECS::Service" } | Select-Object -First 1).PhysicalResourceId
+
+aws ecs update-service --cluster $ecsCluster --service $ecsService --force-new-deployment --region $region
+Write-Host "ECS deployment triggered."
